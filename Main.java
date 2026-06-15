39061cda19fc0180213030c8b46d86113b8109f7
Harshini <harshini.muruganathan@gmail.com>
2026-06-15 19:32:32 +0530

    Ques 3

--- Main.java
+++ Main.java
@@ -5,12 +5,24 @@
     }
 } */
 
-public class Main{
+/* 2) public class Main{
     public static void main(String[] args){
         System.out.println(-5+8*6);
         System.out.println((55+9)%9 );
         System.out.println(20+(-3*5)/8);
         System.out.println(5+15/3*2-8%3);
+    }
+}
+    */
+
+public class Main{
+    public static void main(String[] args){
+        double r=7;
+        double area=3.14*r*r;
+        double perimeter=2*3.14*r;
+        System.out.println("Area of circle is: "+area);
+        System.out.println("Perimeter of circle is: "+perimeter);
+    
     }
 }
 
