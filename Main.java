cdd23152d4fc0afff7196cc03e66577c12fbfd8a
Harshini <harshini.muruganathan@gmail.com>
2026-06-15 19:37:24 +0530

    Ques 4

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@
 }
     */
 
-public class Main{
+/* 3) public class Main{
     public static void main(String[] args){
         double r=7;
         double area=3.14*r*r;
@@ -26,3 +26,21 @@ public class Main{
     }
 }
 
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        String bin1 = sc.next();
+        System.out.print("Input second binary number: ");
+        String bin2 = sc.next();
+        int num1 = Integer.parseInt(bin1, 2);
+        int num2 = Integer.parseInt(bin2, 2);
+        int sum = num1 + num2;
+        String result = Integer.toBinaryString(sum);
+        System.out.println("Sum of two binary numbers: " + result);
+        sc.close();
+    }
+}
